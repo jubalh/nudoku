016e69390aaf5576f923f5bca94d0969e2646b39
Michael Vetter <jubalh@openmailbox.org>
2015-08-21 18:21:17 +0200

    Remove non existing function from headers

--- src/sudoku.h
+++ src/sudoku.h
@@ -7,7 +7,6 @@ typedef enum { D_EASY, D_NORMAL, D_HARD } DIFFICULTY;
 
 int		get_holes(DIFFICULTY level);
 char*	difficulty_to_str(DIFFICULTY level);
-bool	board_is_equal(int one[9][9], int two[9][9]);
 char*	generate_puzzle(int holes);
 int		solve(char puzzle[STREAM_LENGTH]);
 bool	is_valid_puzzle(char puzzle[STREAM_LENGTH]);
