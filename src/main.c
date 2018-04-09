f5911ffbb0964eeeb472fe8d067ad32ec9f1cd3d
Michael Vetter <jubalh@iodoru.org>
2018-04-09 16:18:28 +0200

    Align includes and defines

--- src/main.c
+++ src/main.c
@@ -19,34 +19,34 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* INCLUDES */
-#include <stdlib.h>		/* rand, srand */
-#include <unistd.h>		/* getopt */
-#include <ncurses.h>	/* ncurses */
-#include <time.h>		/* time */
-#include <string.h>		/* strcmp, strlen */
-#include "sudoku.h"		/* sudoku functions */
+#include <stdlib.h>				/* rand, srand */
+#include <unistd.h>				/* getopt */
+#include <ncurses.h>			/* ncurses */
+#include <time.h>				/* time */
+#include <string.h>				/* strcmp, strlen */
+#include "sudoku.h"				/* sudoku functions */
 
 /* DEFINES */
 //#define VERSION				"0.1" //gets set via autotools
-#define GRID_LINES			19
-#define GRID_COLS			37
-#define GRID_Y				3
-#define GRID_X				3
-#define INFO_LINES			19
-#define INFO_COLS			20
-#define INFO_Y				3
-#define INFO_X				GRID_X + GRID_COLS + 5
-#define GRID_NUMBER_START_Y 1
-#define GRID_NUMBER_START_X 2
-#define GRID_LINE_DELTA		4
-#define GRID_COL_DELTA		2
-#define STATUS_LINES		1
-#define STATUS_COLS			GRID_COLS + INFO_COLS
-#define STATUS_Y			1
-#define STATUS_X			GRID_X
-#define MAX_HINT_RANDOM_TRY	20
-#define SUDOKU_LENGTH		STREAM_LENGTH - 1
-#define COLOR_HIGHLIGHT		4
+#define GRID_LINES				19
+#define GRID_COLS				37
+#define GRID_Y					3
+#define GRID_X					3
+#define INFO_LINES				19
+#define INFO_COLS				20
+#define INFO_Y					3
+#define INFO_X					GRID_X + GRID_COLS + 5
+#define GRID_NUMBER_START_Y		1
+#define GRID_NUMBER_START_X		2
+#define GRID_LINE_DELTA			4
+#define GRID_COL_DELTA			2
+#define STATUS_LINES			1
+#define STATUS_COLS				GRID_COLS + INFO_COLS
+#define STATUS_Y				1
+#define STATUS_X				GRID_X
+#define MAX_HINT_RANDOM_TRY		20
+#define SUDOKU_LENGTH			STREAM_LENGTH - 1
+#define COLOR_HIGHLIGHT			4
 #define COLOR_HIGHLIGHT_CURSOR	5
 
 #ifdef DEBUG
@@ -56,7 +56,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 /* GLOBALS */
 static bool g_useColor = true;
 static bool g_playing = false;
-static char* g_provided_stream; /* in case of -s flag the user provides the sudoku stream */
+static char* g_provided_stream;				/* in case of -s flag the user provides the sudoku stream */
 static char plain_board[STREAM_LENGTH];
 static char user_board[STREAM_LENGTH];
 static DIFFICULTY g_level = D_EASY;
