678d1e5520f7dab1234d4decb8310090fc7d4290
Michael Vetter <jubalh@iodoru.org>
2019-04-09 16:49:02 +0200

    Print sudoku into png
    Now we pass a stream to generate_output(). We then print a sudoku grid
    and put the numbers inside.

--- src/outp.c
+++ src/outp.c
@@ -20,6 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 /* INCLUDES */
 #include <cairo.h>
+#include <stdio.h>
 
 /* FUNCTIONS */
 void generate_output(char *stream)
@@ -28,15 +29,58 @@ void generate_output(char *stream)
 	surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 600, 600);
 	cr = cairo_create(surface);
 
-	cairo_set_source_rgb(cr, 0, 0, 0); cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+	cairo_set_source_rgb(cr, 0, 0, 0);
+	cairo_set_line_cap(cr, CAIRO_LINE_CAP_SQUARE);
+	/*
+	cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
 	cairo_set_font_size(cr, 10.0);
 
 	cairo_move_to(cr, 5.0, 20.0); cairo_show_text(cr, "Nudoku");
 
-	cairo_set_line_width(cr, 5); cairo_set_line_cap(cr, CAIRO_LINE_CAP_SQUARE);
+	cairo_set_line_width(cr, 5);
 	cairo_move_to(cr, 5.0, 70);
 	cairo_line_to(cr, 455, 70);
 	cairo_stroke(cr);
+	*/
+
+	cairo_set_line_width(cr, 2);
+	for (int x=0; x < 9; x++)
+	{
+		for (int y=0; y < 9; y++)
+		{
+			cairo_rectangle(cr, 30 + x*50, 30 + y*50, 50, 50);
+		}
+	}
+	cairo_stroke(cr);
+
+	cairo_set_line_width(cr, 4);
+
+	for (int x=0; x < 3; x++)
+	{
+		for (int y=0; y < 3; y++)
+		{
+			cairo_rectangle(cr, 30 + x*150, 30 + y*150, 150, 150);
+		}
+	}
+	cairo_stroke(cr);
+
+	cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+	cairo_set_font_size(cr, 25.0);
+	int c;
+	char s[4];
+	for(int row=0; row < 9; row++)
+	{
+		for(int	col=0; col < 9; col++)
+		{
+			c = stream[row*9+col];
+			if (c != '.')
+			{
+				cairo_move_to(cr, 45 + 50*row, 60 + 50*col);
+				sprintf(s, "%c", c);
+				cairo_show_text(cr, s);
+			}
+		}
+	}
 
 	cairo_surface_write_to_png(surface, "image.png");
 	cairo_destroy(cr);
